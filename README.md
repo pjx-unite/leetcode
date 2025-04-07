04d0bcbb789d7dc0a1d0c5fb5c18d9221d84a465
Booker <wurocpengpeng@gmail.com>
2025-04-07 23:33:22 +0800

    突击-hash

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 [[977]SquaresOfASortedArray.py](src/leetcode/editor/en/SquaresOfASortedArray.py)</br>
 [[1004]MaxConsecutiveOnesIII.py](src/leetcode/editor/en/MaxConsecutiveOnesIII.py)</br>
 [[1480]RunningSumOf1dArray.py](src/leetcode/editor/en/RunningSumOf1dArray.py)</br>
+##hash
+[[1832]CheckIfTheSentenceIsPangram.py](src/leetcode/editor/en/CheckIfTheSentenceIsPangram.py)
--- src/leetcode/editor/en/[3]LongestSubstringWithoutRepeatingCharacters.py
+++ src/leetcode/editor/en/[3]LongestSubstringWithoutRepeatingCharacters.py
@@ -6,5 +6,12 @@ from collections import Counter
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+        left  = ans = 0
+        dict = defaultdict(int)
+        for right in range(len(s)):
+            if s[right] in dict:
+                left = max(left, dict[s[right]])
+            ans = max(ans, right - left + 1)
+            dict[s[right]] = right + 1
+        return ans
 # leetcode submit region end(Prohibit modification and deletion)
